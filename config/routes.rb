c031621d57b22b67550ffa31f56bcfa12ccc63e5
Blake Hill <blhill@redventures.com>
2021-05-22 14:39:32 -0400

    added additional crud  logic to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#homepage'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
